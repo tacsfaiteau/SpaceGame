c454a03c82e414d6df2c4d958ac63fcaf8f742dc
tacsfaiteau <55443318+tacsfaiteau@users.noreply.github.com>
2020-03-10 07:48:33 -0400

    Finished

---
+++ SpaceGame/Assets/AsteriodSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteriodSpawner : MonoBehaviour
+{
+    public GameObject asteriodPrefab;
+    public Vector3[] spawnPos;
+    public float spawnInterval = 1;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval)
+        {
+            int e = Random.Range(0, 3);
+            List<GameObject> wave = new List<GameObject>();
+            for (int i = 0; i < spawnPos.Length; i++)
+            {
+                GameObject asteriod = Instantiate<GameObject>(asteriodPrefab, spawnPos[i], Quaternion.identity);
+                asteriod.name = "Asteriod";
+                wave.Add(asteriod);
+            }
+            wave[e].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+            wave[e].name = "Points";
+
+            timer = 0;
+            spawnInterval = Random.Range(0.8f, 1.2f);
+        }
+    }
+}
---
+++ SpaceGame/Assets/Rocket.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Rocket : MonoBehaviour
+{
+    public TextMeshProUGUI points;
+    public TextMeshProUGUI health;
+
+    public Vector3[] rocketPos;
+    public int currPosIndex = 1;
+    public float t = 0;
+    public float moveSpeed = 7;
+    public bool isAlive = true;
+    public bool isFlinching = false;
+    public int hp = 4;
+    public int score = 0;
+    public float flinchDur = 5;
+    public float ft = 0;
+    public SpriteRenderer s;
+    public Color flinchColor = new Color(0.5f, 0, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<SpriteRenderer>();
+        health.text = "HP" + hp;
+        points.text = "Score" + score;
+    }
+
+    void killPlayer()
+    {
+        s.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+       if(col.gameObject.name == "Asteriod")
+        {
+            hp = hp - 1;
+            isFlinching = true;
+            if(hp < 0)
+            {
+                hp = 0;
+                isAlive = false;
+            }
+            health.text = "HP" + hp;
+        }if(col.gameObject.name == "Points" &! isFlinching && isAlive)
+        {
+            score = score + 1;
+            points.text = "Score" + score;
+        }
+        if (!isAlive)
+        {
+            killPlayer();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFlinching && ft < flinchDur)
+        {
+            ft += Time.deltaTime;
+
+            s.color = Color.Lerp(Color.white, flinchColor, Mathf.PingPong(ft, 0.5f));
+        }else if(ft > flinchDur)
+        {
+            ft = 0;
+            isFlinching = false;
+        }
+        t = Time.deltaTime * moveSpeed;
+        transform.position = Vector3.Lerp(transform.position, rocketPos[currPosIndex], t);
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currPosIndex = currPosIndex + 1;
+            if (currPosIndex > rocketPos.Length - 1)
+            {
+                currPosIndex = rocketPos.Length - 1;
+            }
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currPosIndex = currPosIndex - 1;
+            if (currPosIndex < 0)
+            {
+                currPosIndex = 0;
+            }
+        }
+    }
+}
